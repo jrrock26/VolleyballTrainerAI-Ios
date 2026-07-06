fbcd7c3d5e7b81ca1743a8e7d92710deb29c5581
Jason Rock <jrrock26@gmail.com>
2026-07-06 13:59:50 -0400

    Fix: remove duplicate Color extension and heightInches binding

--- VolleyballTrainerAI/PlayerProfileView.swift
+++ VolleyballTrainerAI/PlayerProfileView.swift
@@ -1,33 +1,6 @@
 import SwiftUI
 import PhotosUI
 
-// MARK: - Color Extension
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let a, r, g, b: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (1, 1, 1, 0)
-        }
-        self.init(
-            .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue: Double(b) / 255,
-            opacity: Double(a) / 255
-        )
-    }
-}
-
 // MARK: - Profile Manager
 
 class ProfileManager: ObservableObject {
@@ -336,10 +309,10 @@ struct PlayerProfileView: View {
     
     private func loadProfile() {
         let saved = profileManager.profile
-        if let h = saved.heightInches, h > 0 {
-            heightFeet = Int(h) / 12
-            heightInchPart = Int(h) % 12
-            heightInches = h
+        if saved.heightInches > 0 {
+            heightFeet = Int(saved.heightInches) / 12
+            heightInchPart = Int(saved.heightInches) % 12
+            heightInches = saved.heightInches
         }
         selectedSkillLevel = saved.skillLevel
         selectedPosition = saved.position
